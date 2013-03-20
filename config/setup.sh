b19e85a52a341f90d6795d3a754b28c9b42526e2
chriscauley <chris@lablackey.com>
2013-03-20 02:13:31 +0000

    updating setup a bit now that I have tried it on a clean linux box

--- config/setup.sh
+++ config/setup.sh
@@ -1,27 +1,15 @@
 suod apt-get update
-sudo apt-get install postgresql-9.1 postgresql-server-dev-9.1 python-dev
-sudo apt-get install git-core emacs23-nox screen mlocate nginx uwsgi uwsgi-plugin-python
+sudo apt-get install postgresql-9.1 postgresql-server-dev-9.1 python-dev git-core \
+    emacs23-nox screen mlocate nginx uwsgi uwsgi-plugin-python python-setuptools node-less -y
+sudo easy_install pip
 
+# need this to push
 git config --global user.email "chris@lablackey.com"
 git config --global user.name "chriscauley"
 
+# caching for memcached, probably depracating that soon in favor of varnish
 sudo mkdir /var/cache/django
 sudo chown chriscauley.chriscauley /var/cache/django/
+
+# python programs
 sudo pip install -r requirements.txt 
-
-# install node
-sudo apt-get update
-sudo apt-get install git-core curl build-essential openssl libssl-dev -y
-git clone https://github.com/joyent/node.git
-cd node
-./configure
-make
-sudo make install
-node -v
-
-curl https://npmjs.org/install.sh | sudo sh
-npm -v
-sudo npm install less
-sudo updatedb
-
-echo "you must now create a symlink from lessc to /usr/bin"

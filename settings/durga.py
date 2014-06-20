10b8901d773c48bb36504d3196d96db0203f93cd
chriscauley <chris@lablackey.com>
2014-06-20 11:50:40 -0400

    adding settings.local and making static a hidden folder

--- settings/durga.py
+++ settings/durga.py
@@ -3,8 +3,4 @@ EMAIL_HOST = 'smtp.mandrillapp.com'
 EMAIL_PORT = 587
 EMAIL_USER = 'chris@lablackey.com'
 EMAIL_HOST_USER = 'chris@lablackey.com'
-EMAIL_HOST_PASSWORD = 'ee0ec710-7374-4a22-a726-703befff68f2'
-EMAIL_PASSWORD = 'ee0ec710-7374-4a22-a726-703befff68f2'
 DEFAULT_FROM_EMAIL = "noreply@txrxlabs.org"
-
-PPATH = "/home/django/placereddit/"

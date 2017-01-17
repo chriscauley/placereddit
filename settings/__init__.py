b710d9f7e0e0ac96ccf4184612dec0186fbee0f4
chriscauley <chris@lablackey.com>
2017-01-17 15:17:00 -0500

    adding title

--- settings/__init__.py
+++ settings/__init__.py
@@ -31,6 +31,16 @@ TEMPLATE_LOADERS = (
   'django.template.loaders.filesystem.Loader',
   'django.template.loaders.app_directories.Loader',
 )
+TEMPLATE_CONTEXT_PROCESSORS = (
+  'django.contrib.auth.context_processors.auth',
+  'django.core.context_processors.debug',
+  'django.core.context_processors.request',
+  'django.core.context_processors.i18n',
+  'django.core.context_processors.media',
+  'django.core.context_processors.static',
+  'django.core.context_processors.tz',
+  'django.contrib.messages.context_processors.messages'
+)
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',

b871d01228fffb6e782a2e98dbd560956aab3440
chriscauley <chris@lablackey.com>
2019-09-02 21:37:35 +0000

    adding logs to management command

--- reddit/management/commands/update_placereddit.py
+++ reddit/management/commands/update_placereddit.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from django.core.mail import mail_admins
 from django.core.management.base import BaseCommand
 from reddit.models import SubReddit
+import datetime
 
 class Command (BaseCommand):
   def handle(self, *args, **options):
     results = []
+    print "UPDATING %s SUBREDDITS"%SubReddit.objects.count(), datetime.datetime.now()
     for s in SubReddit.objects.all():
       images = s.pull_from_imgur()
       if images:
         results.append("%s images %s"%(s,len(images)))
+    print "DONE"

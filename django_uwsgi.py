57cb3e57dbd6c3b54ad5a039128888809336763d
chriscauley <chris@lablackey.com>
2019-09-02 21:40:48 +0000

    other minor tweaks

--- django_uwsgi.py
+++ django_uwsgi.py
@@ -1,6 +1,7 @@
 import os
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
 
 os.environ['PYTHON_EGG_CACHE'] = '/tmp/egg_cache'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-application = django.core.handlers.wsgi.WSGIHandler()
+
+application = get_wsgi_application()
